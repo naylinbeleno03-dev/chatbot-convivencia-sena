40bedc51e9d95d7476ba00752bcbb41323ba8943
naylinbeleno03-dev <naylinbeleno03@gmail.com>
2026-09-13 21:01:04 -0500

    Update app.py

--- app.py
+++ app.py
@@ -430,15 +430,15 @@ with st.sidebar:
         api_key = st.text_input("Clave API de Gemini", type="password")
 
 # Selector de Rol del Usuario
-user_role = st.selectbox(
-    "Perfil del Consultante:",
-    ["Estudiante", "Acudiente / Padre de Familia", "Docente / Directivo"],
+        user_role = st.selectbox(
+             "Perfil del Consultante:",
+             ["Estudiante", "Acudiente / Padre de Familia", "Docente / Directivo"],
         )
 
         st.sidebar.markdown("---")
         st.sidebar.markdown("### Biblioteca Virtual")
         st.sidebar.markdown(
-    "[Abrir Repositorio Digital](https://repositorioconvivencia-k3hz5bcgykhxqwn6gn7cjh.streamlit.app/)"
+            "[Abrir Repositorio Digital](https://repositorioconvivencia-k3hz5bcgykhxqwn6gn7cjh.streamlit.app/)"
         )
     
     # Consulta de Marco Legal e Institucional con ENLACES DIRECTOS
